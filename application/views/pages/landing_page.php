5809e98c9faa9d175855526675baac167536f74c
thoriqdharmawan <thoriqd21@gmail.com>
2023-06-27 19:37:51 +0700

    chore: implement templates providers

--- application/views/pages/landing_page.php
+++ application/views/pages/landing_page.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Todo App - Landing Page</title>
-</head>
-<body>
-    <h1>Welcome to Todo App</h1>
-    <p>Start managing your tasks and projects efficiently.</p>
-    <a href="<?php echo base_url('dashboard'); ?>">Go to Dashboard</a>
-</body>
-</html>
+<h1>Welcome to Todo App</h1>
+<p>Start managing your tasks and projects efficiently.</p>
+<a href="<?php echo base_url('dashboard'); ?>">Go to Dashboard</a>
