3fc0c1dcfa6b20e93c65d84d8d4e29c3336568b0
Thoriq Dharmawan <thoriq.dharmawan-guest@moladin.com>
2023-06-28 19:31:04 +0700

    chore: change title

--- application/views/pages/landing_page.php
+++ application/views/pages/landing_page.php
@@ -1,3 +1,3 @@
-<h1>Welcome to Todo App</h1>
+<h1>Selamat datang di Todo App</h1>
 <p>Start managing your tasks and projects efficiently.</p>
 <a href="<?php echo base_url('dashboard'); ?>">Go to Dashboard</a>
