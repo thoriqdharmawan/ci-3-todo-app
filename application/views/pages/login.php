9cc556e6e66c25edd3ce709669a62d5c77fa98bc
Thoriq Dharmawan <thoriq.dharmawan-guest@moladin.com>
2023-07-14 17:07:53 +0700

    feat: add default value

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -8,19 +8,20 @@
     <div class="card">
       <div class="card-body login-card-body">
         <p class="login-box-msg">Silahkan Masuk</p>
+        <p class="alert alert-primary">Email: admin | Password: admin</p>
 
         <?php echo validation_errors(); ?>
         <?php echo $this->session->flashdata('error'); ?>
         <?php echo form_open('auth/login'); ?>
           <div class="input-group mb-3">
-            <input type="text" name="username" id="username" placeholder="Username" class="form-control" required>
+            <input value="admin" type="text" name="username" id="username" placeholder="Username" class="form-control" required>
             <div class="input-group-append">
               <span class="fa fa-envelope input-group-text"></span>
             </div>
           </div>
 
           <div class="input-group mb-3">
-            <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
+            <input value="admin" type="password" name="password" id="password" class="form-control" placeholder="Password" required>
             <div class="input-group-append">
               <span class="fa fa-lock input-group-text"></span>
             </div>
